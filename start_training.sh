63ffb5c58ba1120c5649c75e4ba11823a8577a97
wu zaiqiang <wuzaiqiang@zju.edu.cn>
2022-07-06 13:48:13 +0900

    add

--- start_training.sh
+++ start_training.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 python tools/train.py \
-    --cfg experiments/deepfashion2/hrnet/top1_only.yaml \
-     MODEL.PRETRAINED models/pose_hrnet-w48_384x288-deepfashion2_mAP_0.7017.pth
+    --cfg experiments/deepfashion2/hrnet/top1_only.yaml
